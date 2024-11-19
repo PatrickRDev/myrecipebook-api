ffb992ab07d77c81251b608e570c26dd5398a7b7
Patrick Rodrigues <pkscriptrodrigues@gmail.com>
2024-11-19 12:00:14 -0300

    chore: estrutura inicial da API MyRecipeBook

---
+++ src/Backend/MyRecipeBook.API/Controllers/Class.cs
@@ -0,0 +1,6 @@
+﻿namespace MyRecipeBook.API.Controllers
+{
+    public class Class
+    {
+    }
+}
---
+++ src/Shared/MyRecipeBook.Communication/Requests/RequestRegisterUserJson.cs
@@ -0,0 +1,13 @@
+﻿namespace MyRecipeBook.Communication.Requests
+{
+    public class RequestRegisterUserJson
+    {
+
+
+        public string email { get; set; } = string.Empty;
+
+        public string password { get; set; } = string.Empty;
+
+        public string name { get; set; } = string.Empty;
+    }
+}
