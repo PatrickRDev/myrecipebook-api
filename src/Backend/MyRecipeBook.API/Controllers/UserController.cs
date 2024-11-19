c59269cfa0aa5edd6c94e276b9fc170d42378c03
Patrick Rodrigues <pkscriptrodrigues@gmail.com>
2024-11-19 12:00:14 -0300

    feat: Implementa caso de uso RegisterUserUseCase no UserController e ajusta referências em Application e Exceptions

---
+++ src/Backend/MyRecipeBook.API/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+//ponto de entrada pra nossa api
+namespace MyRecipeBook.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        //o producesresponsetype é 
+        [ProducesResponseType(typeof(ResponseRegisterUserJson),StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterUserJson request)
+        {
+            return Created();
+        }
+    }
+}
+
+
