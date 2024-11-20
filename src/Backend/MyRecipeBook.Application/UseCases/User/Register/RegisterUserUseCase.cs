76a53e40f4230e5d1289b5324f921f013193dc26
Patrick Rodrigues <pkscriptrodrigues@gmail.com>
2024-11-20 19:19:18 -0300

    Implementa caso de uso RegisterUserUseCase no UserController e ajusta referencias em Aplication e Exceptions

---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -0,0 +1,34 @@
+﻿using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.Application.UseCases.User.Register
+{
+    public class RegisterUserUseCase
+    {
+        public  ResponseRegisterUserJson Execute(RequestRegisterUserJson request)
+        {
+            Validate(request);
+            //validar request
+            //mapear a request em uma entidade
+            //criptografia senha
+            //salvar no banco de dados
+            return new ResponseRegisterUserJson { Name = request.name };
+        }
+
+
+        private void Validate(RequestRegisterUserJson request) 
+        {
+            var validator = new RegisterUserValidator();
+            var result= validator.Validate(request);
+
+            if(result.IsValid == false)
+            {
+                var errorMessages = result.Errors.Select (x => x.ErrorMessage);
+                throw new Exception();
+            }
+
+            
+
+        }
+    }
+}
