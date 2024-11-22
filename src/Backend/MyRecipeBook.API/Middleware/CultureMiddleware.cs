dddbe0fe3d0a303966d820bc29c0afac27be146b
Patrick Rodrigues <pkscriptrodrigues@gmail.com>
2024-11-22 17:04:35 -0300

    feat: Implementa middleware de cultura para internacionalização

---
+++ src/Backend/MyRecipeBook.API/Middleware/CultureMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+
+namespace MyRecipeBook.API.Middleware
+{
+    public class CultureMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CultureMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            var supportedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures);
+
+            var reqestedCulture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+
+            var cultureinfo = new CultureInfo("en");
+
+            if(string.IsNullOrEmpty(reqestedCulture) == false && supportedLanguages.Any(c => c.Name.Equals(reqestedCulture)))
+            {
+                cultureinfo = new CultureInfo(reqestedCulture);
+            }
+
+            CultureInfo.CurrentCulture = cultureinfo;
+            CultureInfo.CurrentUICulture = cultureinfo;
+
+            await _next(context);
+        }
+    }
+}
