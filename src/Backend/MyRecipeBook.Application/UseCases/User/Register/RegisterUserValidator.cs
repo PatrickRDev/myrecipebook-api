15de4dd36d705017cbe5e422dabbcd3fd058fc91
Patrick Rodrigues <pkscriptrodrigues@gmail.com>
2024-11-22 17:02:10 -0300

    feat: Adiciona validações no RegisterUserValidator usando FluentValidation

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -9,9 +9,9 @@ namespace MyRecipeBook.Application.UseCases.User.Register
         public RegisterUserValidator()
         {
             RuleFor(x => x.name).NotEmpty().WithMessage(ResourceMessagesException.NAME_EMPTY);
-            RuleFor(x => x.email).NotEmpty();
-            RuleFor(x => x.email).EmailAddress().WithMessage("Nao é um email válido");
-            RuleFor(x => x.password.Length).GreaterThanOrEqualTo(6);
+            RuleFor(x => x.email).NotEmpty().WithMessage(ResourceMessagesException.EMAIL_EMPTY);
+            RuleFor(x => x.email).EmailAddress().WithMessage(ResourceMessagesException.EMAIL_INVALID);
+            RuleFor(x => x.password.Length).GreaterThanOrEqualTo(6).WithMessage(ResourceMessagesException.PASSWORD_EMPTY);
         }
     }
 }
